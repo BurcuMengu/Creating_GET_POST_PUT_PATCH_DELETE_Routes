4c377b549f1a572a4370dbb58e00bdb329cb405f
BurcuMengu <burcumengu@gmail.com>
2024-06-12 18:04:42 +0300

    push -u origin main

--- index.js
+++ index.js
@@ -35,6 +35,33 @@ app.post("/jokes", (req, res) => {
   res.json(newJoke);
 });
 
+app.put("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const replacementJoke = {
+    jokeText: req.body.text,
+    JokeType: req.body.type,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+
+  jokes[searchIndex] = replacementJoke;
+  res.json(replacementJoke);
+});
+
+app.patch("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const existingJoke = jokes.find((joke) => joke.id === id);
+  const replacementJoke = {
+    id: id,
+    jokeText: req.body.text || existingJoke.jokeText,
+    jokeType: req.body.type || existingJoke.jokeType,
+  };
+
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  jokes[searchIndex] = replacementJoke;
+  console.log(jokes[searchIndex]);
+  res.json(replacementJoke);
+});
 
 
 app.listen(port, () => {
