971b76c60640ea7ac5dc259eeb0a9ae656f589db
BurcuMengu <burcumengu@gmail.com>
2024-06-13 17:20:37 +0300

    push -u origin main

--- index.js
+++ index.js
@@ -63,6 +63,31 @@ app.patch("/jokes/:id", (req, res) => {
   res.json(replacementJoke);
 });
 
+app.delete("/jokes/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const searchIndex = jokes.findIndex((joke) => joke.id === id);
+  if (searchIndex > -1) {
+    jokes.splice(searchIndex, 1);
+    res.sendStatus(200);
+  } else {
+      res
+      .status(404)
+      .json({ error: `Jpke with id: ${id} not found. No jokes were deleted.` });
+  }
+});
+
+app.delete("/all", (req, res) => {
+  const userKey = req.body.key;
+  if (userKey === masterKey) {
+    jokes = [];
+    res.status(200);
+  } else {
+    res
+    .status(404)
+    .json({ error: `You are not authorised to perform this action.` });
+  }
+});
+
 
 app.listen(port, () => {
     console.log(`Successfully started server on port ${port}`);
